f7bac79968b83ae84057b6d8241bff82465e0b23
Manuel Sandoval Mendoza <msandovalm01@gmail.com>
2026-02-05 11:19:44 -0600

    added dog detail layout

---
+++ app/src/main/java/com/sanddev/doggodex/dogdetail/DogDetailActivity.kt
@@ -0,0 +1,15 @@
+package com.sanddev.doggodex.dogdetail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sanddev.doggodex.R
+
+class DogDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
